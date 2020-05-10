1177455f3787592ad195b2509a44f3b8dbdf950d
CianDoyleTUD <57116401+CianDoyleTUD@users.noreply.github.com>
2020-05-10 18:13:21 +0100

    Update PrimLists.java

--- PrimLists.java
+++ PrimLists.java
@@ -3,81 +3,7 @@
 
 import java.io.*;
 import java.util.ArrayList;
-
-class Heap
-{
-    private int[] a;	   // heap array
-    private int[] hPos;	   // hPos[a[k]] == k
-    private int[] dist;    // dist[v] = priority of v
-
-    private int N;         // heap size
-   
-    // The heap constructor gets passed from the Graph:
-    //    1. maximum heap size
-    //    2. reference to the dist[] array
-    //    3. reference to the hPos[] array
-    public Heap(int maxSize, int[] _dist, int[] _hPos) 
-    {
-        N = 0;
-        a = new int[maxSize + 1];
-        dist = _dist;
-        hPos = _hPos;
-    }
-
-
-    public boolean isEmpty() 
-    {
-        return N == 0;
-    }
-
-
-    public void siftUp( int k) 
-    {
-        int v = a[k];
-
-        // code yourself
-        // must use hPos[] and dist[] arrays
-    }
-
-
-    public void siftDown( int k) 
-    {
-        int v, j;
-       
-        v = a[k];  
-        
-        // code yourself 
-        // must use hPos[] and dist[] arrays
-    }
-
-
-    public void insert( int x) 
-    {
-        a[++N] = x;
-        siftUp( N);
-    }
-
-
-    public int remove() 
-    {   
-        int v = a[1];
-        hPos[v] = 0; // v is no longer in heap
-                
-        a[1] = a[N--];
-        siftDown(1);
-        
-        a[N+1] = 0;  // put null node into empty spot
-        
-        return v;
-    }
-
-    public void swap(int x, int y) {
-        int temp = a[x];
-        a[x] = a[y];
-        a[y] = temp;
-    }
-
-}
+import java.util.Stack; 
 
 class Graph {
     class Node {
@@ -85,7 +11,7 @@ class Graph {
         public int wgt;
         public Node next;
     }
-
+    // Edge class used for minimum spanning tree
     class Edge {
         public int vert1;
         public int vert2;
@@ -107,7 +33,9 @@ class Graph {
     // used for traversing graph
     private ArrayList<Integer> toVisit = new ArrayList<Integer>();
     private int[] visited;
-    private int id;
+
+    // used for iterative version of depth-first
+    private Stack<Integer> dfStack;
     
     
     // default constructor
@@ -123,13 +51,15 @@ class Graph {
         String splits = " +";  // multiple whitespace as delimiter
 		String line = reader.readLine();        
         String[] parts = line.split(splits);
-        System.out.println("Vertices: " + parts[0] + " Edges: " + parts[1]);
+        System.out.println("Graph succesfully loaded.\nVertices: " + parts[0] + " Edges: " + parts[1]);
         
         V = Integer.parseInt(parts[0]);
         E = Integer.parseInt(parts[1]);
-
+        // Initialise adjacency matrix to appropriate size
         adjMatrix = new int[V+1][V+1];
-        
+
+        visited = new int[V+1];
+
         // create sentinel node
         z = new Node(); 
         z.next = z;
@@ -178,26 +108,30 @@ class Graph {
     }
     
     // method to display the graph representation
-    public void display() {
+    public void display() 
+    {
+
         int v;
         Node n;
 
         System.out.println("\nAdjacency matrix\n"); 
 
         System.out.print("   ");
+
         for(int i = 1; i <= V; i++){
             System.out.print(toChar(i) + " ");
         }
 
         System.out.println("");
         
+        // Loop through adjacency matrix and print values
         for(int i = 1; i <= V; i++)
         {
             for(int j = 1; j <= V; j++)
             {
                 if (j == 1)
                     System.out.print(toChar(i) + "| ");
-                if(adjMatrix[i][j] == 0) // Looks better than printing null
+                if(adjMatrix[i][j] == 0) // prints null otherwise, which ruins formatting
                     System.out.print("0 ");   
                 else
                     System.out.print(adjMatrix[i][j] + " "); 
@@ -205,9 +139,11 @@ class Graph {
             System.out.println("");
         }
 
-        for(v=1; v<=V; ++v)
+        // Print adjacency lists
+        for(v = 1; v <= V; ++v)
         {
             System.out.print("\nAdjacency list for [" + toChar(v) + "] ->" );
+
             for(n = adj[v]; n != z; n = n.next) 
                 System.out.print(" (" + toChar(n.vert) + " | " + n.wgt + ") ->");    
         }
@@ -217,18 +153,17 @@ class Graph {
 	public void MST_Prim(int s)
 	{
         
-        visited = new int[V+1];
+        //visited = new int[V+1];
         mstWeight = 0;
 
         for(int v = 1; v <= V; v++)
             toVisit.add(v);
 
-        
         visited[s] = 1; // Adding our starting point into list of visted nodes.
 
         toVisit.remove(Integer.valueOf(s)); // No longer consider this node since it's in our tree
 
-        System.out.println("Prim's algorithm");
+        System.out.println("\n---Prim's algorithm---\n");
         System.out.println("Starting at vertex " + toChar(s));
 
         // Keep building MST until all nodes are visited
@@ -252,15 +187,17 @@ class Graph {
 
         for(int v = 1; v <= V; v++)
         {
-            if(visited[v] == 1)// If visited, consider the edges from node
+            if(visited[v] == 1)// If node is in our tree, consider the edges from node
             { 
                 System.out.print("\nPossible connections from [" + toChar(v) + "] -> ");
-                for(n = adj[v]; n != z; n = n.next) 
+
+                for(n = adj[v]; n != z; n = n.next) // Print neighbours
                 {
-                    if(toVisit.contains(n.vert)) // Check all remaining nodes in graph which are unvisited
+                    if(toVisit.contains(n.vert)) // If the neighbour is not in MST yet
                     {
                         System.out.print("(Node " + toChar(n.vert) + " | Weight: " + n.wgt + ") - ");
-                        if(n.wgt < weight) // Finding connection with least weight
+
+                        if(n.wgt < weight) // Finding node connected with least weight
                         {   
                             weight = n.wgt;
                             viableNode = n.vert;
@@ -271,13 +208,13 @@ class Graph {
             }
         }
 
-        mstWeight += weight;
+        mstWeight += weight; // Keep track of total weight of spanning tree
 
         e.vert2 = viableNode;
         e.weight = weight;
 
-        System.out.println("\nAdding node " + toChar(viableNode) + " to minimum spanning tree");
-        System.out.println(mstWeight);
+        System.out.println("\nAdding node " + toChar(viableNode) + " to minimum spanning tree with weight " + weight + " from " + toChar(e.vert1));
+        System.out.println("Current spanning tree weight: " + mstWeight);
 
         // Mark the closest node as part of the tree
         visited[viableNode] = 1; 
@@ -288,7 +225,7 @@ class Graph {
     
     public void showMST()
     {
-        System.out.print("\n\nMinimum Spanning edges, in order:\n");
+        System.out.print("\n\nEdges of spanning tree, in the order they were chosen:\n");
 
         for(int i = 0; i < mst.size(); i++) {   
             System.out.print("{" + toChar(mst.get(i).vert1) + " -> " + toChar(mst.get(i).vert2) + "} Weight: " + mst.get(i).weight + "\n");
@@ -297,20 +234,81 @@ class Graph {
         System.out.println("Total MST weight: " + mstWeight);
     }
 
+    public void DF(int s) // Recursive version
+    {
+
+        Node n;
+        n = adj[s]; 
+
+        visited[s] = 1;
+
+        System.out.print("\n Current node [" + toChar(s) + "]\n Visited nodes:" );
+
+        for(int i = 1; i <= V; i++) 
+        {
+            System.out.print("\n " + toChar(i) + ": " + visited[i]);
+        }
+        
+        for(n = adj[s]; n != z; n = n.next){
+
+            if(visited[n.vert] == 0)
+                DF(n.vert);    
+
+        }
+           
+    }
+
+    public void DF_iteration(int s) // Iterative version
+    {
+
+        dfStack = new Stack<Integer>(); // Create a new stack
+        dfStack.push(s); // Push root node onto top of stack
+
+        int top;
+        Node n;
+
+        while(dfStack.empty() == false) 
+        {
+            top = dfStack.peek(); // Get value at the top of the stack
+            dfStack.pop();
+
+            n = adj[top];
+            
+            if(visited[top] == 0)
+                visited[top] = 1; // Mark node as visited
+
+            System.out.print("\n Current node on top of stack [" + toChar(top) + "]\n Visited nodes:" );
+
+            for(int i = 1; i <= V; i++) 
+            {
+                System.out.print("\n " + toChar(i) + ": " + visited[i]);
+            }
+            
+            for(n = adj[top]; n != z; n = n.next){
+
+                if(visited[n.vert] == 0)
+                    dfStack.push(n.vert); // Push node onto top of stack
+
+            }
+        }
+
+        System.out.print("\n Current node on top of stack [" + toChar(top) + "]\n Visited nodes:" );
+    }
+
 }
 
 public class PrimLists {
     public static void main(String[] args) throws IOException
     {
-        int s = 4;
-        String fname = "myGraph.txt";               
+        int s = 1;
+        String fname = "dfGraph.txt";               
 
         Graph g = new Graph(fname);
        
         //g.display();
 
-       //g.DF(s);
-       //g.DF_iteration(s);
-       g.MST_Prim(s);                  
+        //g.DF(s);
+        g.DF_iteration(s);
+        //g.MST_Prim(s);                  
     }
 }
